3200fd56df8a511747e57470fb7ed36879e147b4
GentleSlap <jrestrepomunoz@gmail.com>
2025-06-17 14:38:15 +0200

    Correction of missing book directory. Refactoring count words to stats.py

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ def get_book_text(path):
         file_content = f.read()
         return file_content
 
-def count_words(text):
-    words = text.split()
-    total_words = len(words)
-    return total_words
-
 
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
+    from stats import count_words
     number_words = count_words(book_text)
     print(f"{number_words} words found in the document")
 
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(text):
+    words = text.split()
+    total_words = len(words)
+    return total_words
